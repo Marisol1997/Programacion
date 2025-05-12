5b2c20959d201d8b0f414e8a0b3e0190d55ac424
Bryan <139916861+Marisol1997@users.noreply.github.com>
2025-05-12 13:54:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,18 @@
-let loadMoreBtn1 = document.querySelector('#load-more-1');
-let currentItem1 = 4;
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
 
-loadMoreBtn1.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-1 .box-1'
-    )];
-    for(var i = currentItem1; i < currentItem1 + 4; i++){
-        boxes[i].style.display = 'inline-block';
-    }
-    currentItem1 += 4;
-    if(currentItem1 >= boxes.length){
-        loadMoreBtn1.style.display = 'none'
-    }
+signIn.onclick = function(){
+    nameInput.style.maxHeight = "0";
+    title.innerHTML = "Login"
+    signUp.classList.add("disable");
+    signIn.classList.remove("disable");
 }
 
-let loadMoreBtn2 = document.querySelector('#load-more-2');
-let currentItem2 = 4;
-
-loadMoreBtn2.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-2 .box-2'
-    )];
-    for(var i = currentItem2; i < currentItem2 + 4; i++){
-        boxes[i].style.display = 'inline-block';
-    }
-    currentItem2 += 4;
-    if(currentItem2 >= boxes.length){
-        loadMoreBtn2.style.display = 'none'
-    }
-}
-
-let loadMoreBtn3 = document.querySelector('#load-more-3');
-let currentItem3 = 4;
-
-loadMoreBtn3.onclick = () => {
-    let boxes = [...document.querySelectorAll(
-        '.box-container-3 .box-3'
-    )];
-    for(var i = currentItem3; i < currentItem3 + 4; i++){
-        boxes[i].style.display = 'inline-block';
-    }
-    currentItem3 += 4;
-    if(currentItem3 >= boxes.length){
-        loadMoreBtn3.style.display = 'none'
-    }
+signUp.onclick = function(){
+    nameInput.style.maxHeight = "60px";
+    title.innerHTML = "Registro"
+    signUp.classList.remove("disable");
+    signIn.classList.add("disable");
 }
